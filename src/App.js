af7e9949508e19cd8591a5f12844ed5320fb28b9
Glory <gauthier.lory@protonmail.com>
2020-12-23 20:05:47 +0100

    call api axios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import FlashcardList from './FlashcardList';
 import './app.css'
+import axios from 'axios'
 
 function App() {
   const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
+
+  useEffect(() =>{
+    axios.get('https://opentdb.com/api.php?amount=10')
+    .then(res =>{
+      setFlashcards(res.data.results.map((questionItem, index) => {
+        const answer = decodeString(questionItem.correct_answer)
+        const options = [
+          ...questionItem.incorrect_answers.map( a => decodeString(a)),
+          answer
+        ]
+        return{
+          id :`${index}-${Date.now()}`,
+          question: decodeString(questionItem.question),
+          answer: questionItem.correct_answer,
+          options: options.sort(() => Math.random() - .1)
+        }
+      }))
+    })
+  }, [])
   return <FlashcardList flashcards={flashcards}/>
+}
+
+function decodeString(str){
+  const textArea = document.createElement('textarea')
+  textArea.innerHTML = str
+  return textArea.value
 }
 
 const SAMPLE_FLASHCARDS = [
